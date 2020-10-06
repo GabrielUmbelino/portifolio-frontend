0c33d791703425cc1d0c7fdf622b7e3e4c5499e4
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2020-10-06 10:21:51 -0300

    improve unit tests for project_stats

--- test/components/project/project_stats.spec.js
+++ test/components/project/project_stats.spec.js
@@ -5,28 +5,26 @@ const projectStats = [
   {
     id: 1,
     description: 'Horas de Trabalho',
-    value: 140,
+    number: 140,
   },
   {
     id: 2,
     description: 'Copos de Café',
-    value: 70,
+    number: 70,
   },
   {
     id: 3,
     description: 'Clientes felizes',
-    value: 1,
+    number: 1,
   },
 ]
 
 describe('Project Stats', () => {
   it('Render Project Stats with values', () => {
     const wrapper = mount(ProjectStats, { propsData: { projectStats } })
-
-    expect(
-      wrapper.find(`.project-stats .stat-1 .primary-title`).text()
-    ).toEqual('Horas de Trabalho')
-
-    expect(wrapper.find(`.project-stats .stat-1 span`).text()).toEqual('140')
+    const description = wrapper.find(`.project-stats .stat-1 .primary-title`)
+    const value = wrapper.find(`.project-stats .stat-1 span`)
+    expect(description.text()).toContain('140')
+    expect(value.text()).toContain('Horas de Trabalho')
   })
 })
